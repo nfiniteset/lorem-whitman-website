bd0fa605e2b11adc4a63346fdc3970f9b18f8f68
Sean Durham <sean@sdurham.net>
2013-06-04 19:25:34 -0700

    Put some Whitman on the page.

--- config.rb
+++ config.rb
@@ -1,51 +1,15 @@
-###
-# Compass
-###
+require 'whitman'
 
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
+DEFAULT_PARAGRAPH_COUNT = 3
+SENTENCES_PER_PARAGRAPH = 3
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def paragraphs(count = DEFAULT_PARAGRAPH_COUNT)
+    count.times.map {
+      Whitman::Paragraph.paragraphs(SENTENCES_PER_PARAGRAPH).join(' ')
+    }
+  end
+end
 
 set :css_dir, 'stylesheets'
 
